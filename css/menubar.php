64bfb8244efe5f91f1e00cd1c92a5988e36e6c17
jasonlee <phantom_138@Hotmail.com>
2015-07-18 13:28:34 -0700

    new css file

---
+++ css/menubar.php
@@ -0,0 +1,107 @@
+<!--
+1. To set it all up visually, let’s zero the default padding (and margin, for older browsers) of the lists:
+2. draw border to see clearly
+3. remove anchor and ul li decoration style
+
+
+
+
+
+
+
+
+
+-->
+<html>
+	<head>
+		
+		
+	</head>
+	
+	
+	
+	<style>
+		#mainmenu a{
+			text-decoration:none;
+		
+		}
+		
+		
+		div{
+
+			background-color:red;
+			border:1px solid black;
+			text-align:center;
+ 
+		}
+		.mainmenu{
+			display: inline-block;
+		}
+		ul{
+			border:1px solid black;
+			list-style:none;
+			margin:0;
+			padding:0;
+			
+			
+		}
+		li{
+			border:1px solid black;
+			padding:5px;
+
+		}
+		
+		
+		/* li parent */
+		.mainmenu > li{
+			float:left;
+		
+			
+		}
+		/* ul child */
+		.mainmenu ul{
+			
+			margin-top:5px;
+			margin-left:-6px;
+			display:none;
+			position:absolute;
+		}
+		
+		.mainmenu li:hover ul{
+			display:block;
+		}
+		
+
+		
+	</style>
+	
+	
+	
+	
+	<body>
+		<div class="menu">
+			<ul class="mainmenu">	
+				<li><a href="#">HOM22222222E</a>
+					<ul>	
+						<li><a href="#">Heelo</a></li>
+						<li><a href="#">Heelossssssss</a></li>
+					</ul>
+				</li>
+				<li><a href="#">HOME</a>
+					<ul>	
+						<li><a href="#">Heel2222222222222222o</a></li>
+						<li><a href="#">Heelo</a></li>
+					</ul>
+				</li>
+				<li><a href="#">HOMEss</a>
+					<ul>	
+						<li><a href="#">Heel2222222222222222o</a></li>
+						<li><a href="#">Heelo</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>	
+	</body>
+
+
+</html>
