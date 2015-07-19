fa84761212754afce4abe7c0f9058754df6eec6c
jasonlee <phantom_138@Hotmail.com>
2015-07-18 23:14:35 -0700

    added ul sample

--- css/centering.php
+++ css/centering.php
@@ -17,14 +17,35 @@ html
 		div{
 			width:100%;
 			height:100%;
-		
+			
+		}
+		ul{
+			
+			margin:0px;
+			padding:0px;
+			text-align:center;
+		}
+		li{
+			padding:5px;
+			display:inline-block;
+			
 		}
 	</style>
 
 	<body>body
 		<h2>Display blocks flaws</h2>
-		<div style="height:500px;">
+		<div style="height:50px;">
+			<ul>
+				<li>HOME </li>
+				<li>ABOUT US</li>
+			</ul>
 		
+		
+		
+		
+		</div>
+		<div style="height:500px;">
+			
 			<div style="height:85%;margin:auto;width:50%;text-align:center;"><span style="width:50%;height:50%;">jasonjasonjasonjasonajsojjasonjasonjasonjasonajsojjasonjasonjasonjasonajsojjasonjasonjasonjasonajsojjasonjasonjasonjasonajsojjasonjasonjasonjasonajsojjasonjasonjasonjasonajsoj</span></div>	
 			<div style="height:30%;">sdsds</div>
 		
