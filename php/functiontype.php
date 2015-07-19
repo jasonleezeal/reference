238d9b8f7f03c0907d0f5f84cec754c464871e06
jasonlee <phantom_138@Hotmail.com>
2015-07-19 12:53:07 -0700

    added new files

---
+++ php/functiontype.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+?>
+
+<html>
+
+	<head>
+
+	</head>
+	
+	<body>
+			<h1>Type of functions</h1>
+			<ul>
+				<li><a href="http://www.w3schools.com/php/php_ref_string.asp" target="_blank">string</a></li>
+				<li><a href="http://www.w3schools.com/php/php_ref_array.asp" target="_blank">array</a></li>
+				<li><a href="http://www.w3schools.com/php/php_ref_mysqli.asp" target="_blank">mysqli</a></li>
+				<li><a href="http://www.w3schools.com/php/php_ref_http.asp" target="_blank">http</a></li>
+				<li><a href="http://www.w3schools.com/php/php_ref_filesystem.asp" target="_blank">filesystem</a></li>
+			</ul>
+	</body>
+
+</html>
