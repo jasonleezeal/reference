5110f5ba79ad6819d3bf7a1e1a8d6931534f5d01
jasonlee <phantom_138@Hotmail.com>
2015-07-18 22:19:03 -0700

    asdads

---
+++ css/centering.php
@@ -0,0 +1,41 @@
+
+<html>
+html
+	<head>
+		
+		head
+	</head>
+	
+	
+	
+	<style>
+		p,h1,div,table,span,a,ul,form,strong,li{
+			border:1px solid black;
+
+		}
+		
+		div{
+			width:100%;
+			height:100%;
+		
+		}
+	</style>
+
+	<body>body
+		<h2>Display blocks flaws</h2>
+		<div style="height:500px;">
+		
+			<div style="height:85%;margin:auto;width:50%;text-align:center;"><span style="width:50%;height:50%;">jasonjasonjasonjasonajsojjasonjasonjasonjasonajsojjasonjasonjasonjasonajsojjasonjasonjasonjasonajsojjasonjasonjasonjasonajsojjasonjasonjasonjasonajsojjasonjasonjasonjasonajsoj</span></div>	
+			<div style="height:30%;">sdsds</div>
+		
+		
+		
+		
+		
+		</div>
+	</body>
+		
+	
+
+
+</html>
---
+++ css/display.php
@@ -0,0 +1,60 @@
+<!--
+1. To set it all up visually, let’s zero the default padding (and margin, for older browsers) of the lists:
+2. draw border to see clearly
+3. remove anchor and ul li decoration style
+
+
+
+
+
+
+
+
+
+-->
+<html>
+html
+	<head>
+		
+		head
+	</head>
+	
+	
+	
+	<style>
+		p,h1,div,table,span,a,ul,form,strong,li,img{
+			border:1px solid black;
+			
+			
+		}
+		
+	
+	</style>
+
+	<body>body
+		<h2>Display blocks flaws</h2>
+		-text will overflow use (word-wrap: break-word)
+		
+		<p>paragraph</p>
+		<h1>header</h1>
+		<div >division</div>
+		<ul><li>unorderlist</li></ul>
+		<li>list</li>
+		<form>Form</form>
+	
+		<img src="https://www.google.com/search?q=images&espv=2&biw=1600&bih=775&source=lnms&tbm=isch&sa=X&ved=0CAYQ_AUoAWoVChMIkPT4wejkxgIVgwOOCh1yTweh#imgrc=kBbyZfdy45J3kM%3A">
+		<h2>Display inlines</h2>
+		-only can take margin left & right<br>
+		-doesnt support width & height ( use display:inline-block)<br>
+		
+		<strong >strong</strong>
+		<a href="#">anchorrrrr</a>
+		<span>span</span>
+		<h2>Display table</h2>
+		<table><tr><td>table</td></tr></table>	
+	</body>
+		
+	
+
+
+</html>
